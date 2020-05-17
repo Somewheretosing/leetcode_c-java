36ee128baaa3fa12ce7d233d6944ab7e833441a0
luan <13122397002@163.com>
2020-05-17 14:28:09 +0800

    leetcode题库 No.71 简化路径

---
+++ C/dirFormat/dirFormat.h
@@ -0,0 +1,10 @@
+char dirFormat(char dir[]); 
+void dirTrim(char dir[]);
+void dirPreProcess1(char dir[]);
+void dirPreProcess2(char dir[]);
+
+#define True (1);
+#define False (0)
+
+
+
---
+++ C/dirFormat/test.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+#include "string.h"
+
+int main()
+{
+    char *a;
+    char b[100];
+    int length;
+    b[1] = 'i';
+    length = strlen(b);
+
+}
+
+
+
